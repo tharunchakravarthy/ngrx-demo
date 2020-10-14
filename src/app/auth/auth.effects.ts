d9e924118d27dc7d204b80b8dca6a646845a1790
tharunchakravarthy <tharunvanam456@gmail.com>
2020-10-15 01:41:23 +0530

    createEffect for login side effect

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,13 +1,23 @@
 import { Injectable } from "@angular/core";
-import { Actions } from "@ngrx/effects";
-
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { AuthActions } from "./actions.types";
+import { tap } from "rxjs/operators";
 @Injectable()
 export class AuthEffects {//should not be injected anywhere as it is only for ngrx
+
+  login$ = createEffect(() => 
+      this.actions$
+      .pipe(
+        ofType(AuthActions.login),//to filter login actions
+        tap(action => {
+          if(action.type == '[Login page] User Login') {
+            localStorage.setItem('user',JSON.stringify(action.user));
+          }
+        })
+      ),
+      {dispatch: false}//infinite loop of login action
+    );
+
   constructor(private actions$: Actions) {
-    actions$.subscribe(action => {
-      if(action.type == '[Login page] User Login') {
-        localStorage.setItem('user',JSON.stringify(action["user"]));
-      }
-    })
   }
 }

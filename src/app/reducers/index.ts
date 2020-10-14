01efafe8df7dc1812752bae0976efc918fa99ed7
tharunchakravarthy <tharunvanam456@gmail.com>
2020-10-15 02:23:45 +0530

    logger meta reducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -15,6 +15,15 @@ export interface AppState {//corresponds to the global state in store
 export const reducers: ActionReducerMap<AppState> = {
   router: routerReducer
 };
+//metareducers - like any reducer but invoked before other reducers
+//arguments - reducers which need to be invoked after this meta reducer
+//retrun output of our reducers
+export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state,action) => {
+    console.log("state before:",state);
+    console.log("action",action);
+    return reducer(state,action);
+  }
+}
 
-
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [logger] : [];

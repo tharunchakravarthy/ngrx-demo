ff7f93ae723b5fa42a6b3665802641412f1ba63f
tharunchakravarthy <tharunvanam456@gmail.com>
2020-10-15 01:29:17 +0530

    nibba implementation of side effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -55,7 +55,8 @@ const routes: Routes = [
     MatToolbarModule,
     AuthModule.forRoot(),
     StoreModule.forRoot(reducers, { metaReducers }),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([])
   ],
   bootstrap: [AppComponent]
 })
